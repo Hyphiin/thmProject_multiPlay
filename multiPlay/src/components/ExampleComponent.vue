d58214c775b1cb1fa35cccadfd9476b4a086d9e4
Chiara <ck@rswz.de>
2022-05-18 12:51:18 +0200

    installed Vue+Quasar
    & cleaned project

---
+++ multiPlay/src/components/ExampleComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ExampleComponent',
+  props: {},
+  setup() {
+    return {};
+  },
+});
+</script>
