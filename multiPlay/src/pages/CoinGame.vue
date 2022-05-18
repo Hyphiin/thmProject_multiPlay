54c3c4edc5858597884bb0d6c0118d4698960dca
Chiara <ck@rswz.de>
2022-05-18 21:43:37 +0200

    wip first CoinGame tests

--- multiPlay/src/pages/CoinGame.vue
+++ multiPlay/src/pages/CoinGame.vue
@@ -1,26 +1,155 @@
 <template>
   <q-page class="row items-center justify-evenly">
-    <div class="game-container"></div>
+    <div ref="gameContainer" class="game-container"></div>
   </q-page>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import { getAuth, signInAnonymously } from 'firebase/auth';
+import { defineComponent, onMounted, ref } from 'vue';
+import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import {
+  getDatabase,
+  ref as storageRef,
+  set,
+  onDisconnect,
+  onValue,
+  onChildAdded,
+  DatabaseReference,
+} from 'firebase/database';
+import { KeyPressListener } from 'components/KeyPressListener';
+
+interface PlayerElements {
+  [key: string]: string | HTMLDivElement;
+}
+interface Players {
+  [key: string]: string | DatabaseEntry;
+}
+
+interface DatabaseEntry {
+  id: string;
+  name: string;
+  direction: string;
+  color: string;
+  x: number;
+  y: number;
+  coins: number;
+}
 
 export default defineComponent({
   name: 'CoinGame',
   components: {},
   setup() {
-    //helper Functions
+    onMounted(() => {
+      initGame();
+    });
+    const db = getDatabase();
+    let playerId: string;
+    let playerRef: DatabaseReference;
+    let players: Players = {};
+    let playerElements: PlayerElements = {};
 
-    // const randomFromArray = (array: [any]): any => {
-    //   return array[Math.floor(Math.random() * array.length)];
-    // };
-    // const getKeyString = (x: number, y: number): string => {
-    //   return `${x}x${y}`;
-    // };
+    // const gameContainer = document.querySelector('.game-container');
+    const gameContainer = ref<HTMLElement>();
 
+    const initGame = () => {
+      new KeyPressListener('ArrowUp', () => handleArrowPress(0, -1));
+      new KeyPressListener('ArrowDown', () => handleArrowPress(0, 1));
+      new KeyPressListener('ArrowRight', () => handleArrowPress(1, 0));
+      new KeyPressListener('ArrowLeft', () => handleArrowPress(-1, 0));
+
+      const allPlayersRef = storageRef(db, 'players');
+      const allCoinsRef = storageRef(db, 'coins');
+
+      //fires when change occurs
+      onValue(allPlayersRef, (snapshot) => {
+        console.log('onValue fired');
+        players = snapshot.val() || {};
+        Object.keys(players).forEach((key) => {
+          const characterState = players[key] as DatabaseEntry;
+          let el = playerElements[key];
+          //update the dom
+          if (el instanceof HTMLDivElement) {
+            el.querySelector('.Character_name')!.textContent =
+              characterState.name;
+            el.querySelector('.Character_coins')!.textContent =
+              characterState.coins.toString();
+            el.setAttribute('data-color', characterState.color);
+            el.setAttribute('data-direction', characterState.direction);
+            const top = 16 * characterState.y - 4 + 'px';
+            const left = 16 * characterState.x + 'px';
+            el.style.transform = `translate3d(${left},${top}, 0)`;
+          }
+        });
+      });
+
+      //fires when a new node is added to the db
+      onChildAdded(allPlayersRef, (snapshot) => {
+        const addedPlayer = snapshot.val();
+        const characterElement = document.createElement('div');
+        characterElement.classList.add('Character', 'grid-cell');
+        if (addedPlayer.id === playerId) {
+          characterElement.classList.add('you');
+        }
+        characterElement.innerHTML = `
+          <div class="Character_shadow grid-cell"></div>
+          <div class="Character_sprite grid-cell"></div>
+          <div class="Character_name-container">
+            <span class="Character_name">hello</span>
+            <span class="Character_coins">0</span>
+          </div>
+          <div class="Character_you-arrow"></div>
+        `;
+
+        playerElements[addedPlayer.id] = characterElement;
+
+        //fill in some initial states
+        if (
+          characterElement instanceof HTMLDivElement &&
+          characterElement !== null
+        ) {
+          characterElement.querySelector('.Character_name')!.textContent =
+            addedPlayer.name;
+          characterElement.querySelector('.Character_coins')!.textContent =
+            addedPlayer.coins;
+          characterElement.setAttribute('data-color', addedPlayer.color);
+          characterElement.setAttribute(
+            'data-direction',
+            addedPlayer.direction
+          );
+          const top = 16 * addedPlayer.y - 4 + 'px';
+          const left = 16 * addedPlayer.x + 'px';
+          characterElement.style.transform = `translate3d(${left},${top}, 0)`;
+          gameContainer.value?.appendChild(characterElement);
+        }
+      });
+    };
+
+    //*****Key Events */
+
+    const handleArrowPress = (xChange: number, yChange: number) => {
+      const tempVar = players[playerId] as DatabaseEntry;
+      const newX = tempVar.x + xChange;
+      const newY = tempVar.y + yChange;
+
+      if (true) {
+        //move next space
+        tempVar.x = newX;
+        tempVar.y = newY;
+        if (xChange === 1) {
+          tempVar.direction = 'right';
+        }
+        if (xChange === -1) {
+          tempVar.direction = 'left';
+        }
+        if (playerRef) {
+          set(playerRef, tempVar);
+        }
+
+        console.log(tempVar);
+      }
+    };
+
+    //*****firebase stuff*****
     const auth = getAuth();
     signInAnonymously(auth)
       .then(() => {
@@ -37,7 +166,89 @@ export default defineComponent({
         );
       });
 
-    return {};
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        // User is signed in, see docs for a list of available properties
+        // https://firebase.google.com/docs/reference/js/firebase.User
+        playerId = user.uid;
+
+        playerRef = storageRef(db, 'players/' + playerId);
+        set(playerRef, {
+          id: playerId,
+          name: createName(),
+          direction: 'right',
+          color: randomFromArray(playerColors),
+          x: 3,
+          y: 10,
+          coins: 0,
+        });
+
+        //remove Player from Firebase, whem disconnect
+        onDisconnect(playerRef).remove();
+      } else {
+        // User is signed out
+        // ...
+      }
+    });
+
+    //*****helper Functions*****
+    const instanceOfDatabaseEntry = (object: any): object is DatabaseEntry => {
+      if (object && object.inputData) {
+        return 'inputData' in object; // true
+      }
+      return false;
+    };
+    const randomFromArray = (array: string[]) => {
+      return array[Math.floor(Math.random() * array.length)];
+    };
+    // const getKeyString = (x: number, y: number): string => {
+    //   return `${x}x${y}`;
+    // };
+
+    //create a randome start name
+    const createName = () => {
+      const prefix = randomFromArray([
+        'COOL',
+        'SUPER',
+        'HIP',
+        'SMUG',
+        'COOL',
+        'SILKY',
+        'GOOD',
+        'SAFE',
+        'DEAR',
+        'DAMP',
+        'WARM',
+        'RICH',
+        'LONG',
+        'DARK',
+        'SOFT',
+        'BUFF',
+        'DOPE',
+      ]);
+      const animal = randomFromArray([
+        'BEAR',
+        'DOG',
+        'CAT',
+        'FOX',
+        'LAMB',
+        'LION',
+        'BOAR',
+        'GOAT',
+        'VOLE',
+        'SEAL',
+        'PUMA',
+        'MULE',
+        'BULL',
+        'BIRD',
+        'BUG',
+      ]);
+      return `${prefix} ${animal}`;
+    };
+    // Options for Player Colors... these are in the same order as our sprite sheet
+    const playerColors = ['blue', 'red', 'orange', 'yellow', 'green', 'purple'];
+
+    return { gameContainer };
   },
 });
 </script>
@@ -99,91 +310,5 @@ button {
 button:active {
   position: relative;
   top: 1px;
-}
-
-/* Characters */
-.grid-cell {
-  position: absolute;
-  width: 16px;
-  height: 16px;
-}
-.Character {
-  transition: transform 0.4s;
-}
-.Character.you {
-  z-index: 1;
-}
-.Character.you .Character_you-arrow {
-  display: block;
-}
-.Character_you-arrow {
-  display: none;
-  position: absolute;
-  top: -18px;
-  left: 5px;
-  width: 7px;
-  height: 5px;
-  background: url(../assets/arrow.png) no-repeat no-repeat;
-}
-.Character_sprite {
-  overflow: hidden;
-  top: -3px;
-  background: url(../assets/characters.png);
-}
-.Character[data-direction='right'] .Character_sprite {
-  background-position-x: 16px;
-}
-.Character[data-color='red'] .Character_sprite {
-  background-position-y: -16px;
-}
-.Character[data-color='orange'] .Character_sprite {
-  background-position-y: -32px;
-}
-.Character[data-color='yellow'] .Character_sprite {
-  background-position-y: -48px;
-}
-.Character[data-color='green'] .Character_sprite {
-  background-position-y: -64px;
-}
-.Character[data-color='purple'] .Character_sprite {
-  background-position-y: -80px;
-}
-.Character_shadow {
-  background: url(../assets/shadow.png) no-repeat no-repeat;
-}
-
-.Character_name-container {
-  position: absolute;
-  top: -12px;
-  left: -5px;
-  font-size: 5px;
-  padding: 1px 2px 1px;
-  border-radius: 3px;
-  background: #333;
-  color: white;
-  font-weight: bold;
-  text-transform: uppercase;
-  white-space: nowrap;
-}
-.Character_coins {
-  margin-left: 1px;
-  color: gold;
-}
-
-/* Coins */
-.Coin_sprite {
-  background: url(../assets/coin.png) no-repeat no-repeat;
-  animation: coinFloat 0.8s linear infinite alternate-reverse;
-}
-@keyframes coinFloat {
-  from {
-    transform: translateY(0);
-  }
-  to {
-    transform: translateY(5px);
-  }
-}
-.Coin_shadow {
-  background: url(../assets/coin-shadow.png) no-repeat no-repeat;
 }
 </style>
