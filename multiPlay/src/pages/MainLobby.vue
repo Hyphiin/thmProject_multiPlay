dceb90fb9eead41f7268baf900a72485b76552d1
Chiara <ck@rswz.de>
2022-05-18 14:38:16 +0200

    wip CoinGame

--- multiPlay/src/pages/MainLobby.vue
+++ multiPlay/src/pages/MainLobby.vue
@@ -1,12 +1,14 @@
 <template>
-  <q-page class="row items-center justify-evenly"> </q-page>
+  <q-page class="row items-center justify-evenly">
+    <div>MOIN!</div>
+  </q-page>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'IndexPage',
+  name: 'MainLobby',
   components: {},
   setup() {
     return {};
