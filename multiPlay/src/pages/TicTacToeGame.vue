d9dd7aa5706a073e10b4ebb0d77d566175ecc002
Nils Friedrich <nfriedrich@pagemachine.de>
2022-07-25 21:04:57 +0200

    more work done tih tictactoe

--- multiPlay/src/pages/TicTacToeGame.vue
+++ multiPlay/src/pages/TicTacToeGame.vue
@@ -26,12 +26,60 @@
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, computed } from 'vue';
+import { defineComponent, ref, onMounted, computed } from 'vue';
+import { getAuth, signInAnonymously, onAuthStateChanged } from 'firebase/auth';
+import {
+  getDatabase,
+  ref as storageRef,
+  set,
+  onDisconnect,
+  onValue,
+  onChildAdded,
+  DatabaseReference,
+  onChildRemoved,
+  update,
+  remove,
+get,
+child,
+} from 'firebase/database';
+import { useRoute } from 'vue-router';
+
+interface Players {
+  [key: string]: DatabaseEntry;
+}
+interface PlayerElements {
+  [key: string]: HTMLDivElement;
+}
+interface DatabaseEntry {
+  id: string;
+  name: string;
+  direction: string;
+  color: string;
+  x: number;
+  y: number;
+  coins: number;
+  value: string;
+}
+interface Coordinates {
+  x: number,
+  y: number
+}
+
 
 export default defineComponent({
   name: 'TicTacToeGame',
   components: {},
   setup() {
+    const db = getDatabase();
+    const route = useRoute()
+
+    let playerId: string;
+    let lobbyId = ref<string>('');
+    let playerRef: DatabaseReference;
+    let playerLobbyRef: DatabaseReference;
+    let players: Players = {};
+    let playerElements: PlayerElements = {};
+
 
     const player = ref('X' )
     const board = ref([
@@ -39,6 +87,10 @@ export default defineComponent({
        ['','',''],
        ['','','']
     ])
+
+    onMounted(() => {
+      initGame();
+    })
 
     const CalculateWinner = (board: Array<string>) => {
       const winningLines = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
@@ -64,6 +116,13 @@ export default defineComponent({
       if(board.value[x][y] !== '') return
 
       board.value[x][y] = player.value
+      var playerValue = player.value
+      const boardRef = storageRef(db, `lobbys/${lobbyId.value}/board/${getKeyString(x, y)}`);
+        set(boardRef, {
+          x,
+          y,
+          playerValue,
+        });
 
       player.value = player.value === 'X' ? 'O' : 'X'
     }
@@ -76,6 +135,40 @@ export default defineComponent({
       ]
       player.value = 'X'
     }
+
+    const initGame = () => {
+
+      if(route.params.lobbyId){
+      lobbyId.value = route.params.lobbyId.toString()
+
+      const allPlayersRef = storageRef(db, `lobbys/${lobbyId.value}/players`);
+      const allBoardRef = storageRef(db, `lobbys/${lobbyId.value}/board`);
+
+      onValue(allPlayersRef, (snapshot) => {
+        players = snapshot.val() || {};
+        Object.keys(players).forEach((key) => {
+          
+        });
+      });
+
+      onChildAdded(allBoardRef, (snapshot) => {
+        const piece = snapshot.val();
+        const key: string = getKeyString(piece.x, piece.y);
+        piece[key] = true;
+
+        
+      });
+
+      onChildRemoved(allBoardRef, (snapshot) => {
+        const { x, y } = snapshot.val();
+        const keyToRemove = getKeyString(x, y);
+      });
+
+    }
+    };
+    const getKeyString = (x: number, y: number): string => {
+      return `${x}x${y}`;
+    };
 
     return { player, board, winner, MakeMove , ResetGame};
   },
