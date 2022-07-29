800910e2d72b65e3ce28ce7043cd3c92ec45634a
Chiara <ck@rswz.de>
2022-07-29 08:03:20 +0200

    wip
    laptop

--- multiPlay/src/pages/TicTacToeGame.vue
+++ multiPlay/src/pages/TicTacToeGame.vue
@@ -2,8 +2,22 @@
   <q-page class="row items-center justify-evenly">
     <div class="player-info">
       <div>
-        <q-input outlined :model-value="playerNameInput" label="Dein Name" bg-color="white"
-          standout="bg-light-green-11 text-black" @update:model-value="(value) => changeName(value)" />
+        <q-input
+          outlined
+          :model-value="playerNameInput"
+          label="Dein Name"
+          bg-color="white"
+          standout="bg-light-green-11 text-black"
+          @update:model-value="(value) => changeName(value)"
+        />
+      </div>
+      <div>
+        <q-btn
+          color="light-green-13"
+          text-color="black"
+          label="Farbe wechseln"
+          @click="changeColor"
+        />
       </div>
     </div>
     <main class="main-container">
@@ -13,11 +27,22 @@
 
       <div class="main-container_board">
         <div v-for="(row, x) in board" :key="x" class="board_div">
-          <div v-for="(cell, y) in row" :key="y" @click="MakeMove(x, y)" class="div_cell"
-            :style="cell === 'X' ? 'color:green' : 'color:red'">
-            <span class="material-symbols-outlined"
-              :style="cell === 'X' ? 'color:rgb(241, 157, 0);' : 'color:rgb(73, 0, 141);'">
-              {{ cell === 'X' ? 'Close' : cell === 'O' ? 'Circle' : ''}}
+          <div
+            v-for="(cell, y) in row"
+            :key="y"
+            @click="MakeMove(x, y)"
+            class="div_cell"
+            :style="cell === 'X' ? 'color:green' : 'color:red'"
+          >
+            <span
+              class="material-symbols-outlined"
+              :style="
+                cell === 'X'
+                  ? 'color:rgb(241, 157, 0);'
+                  : 'color:rgb(73, 0, 141);'
+              "
+            >
+              {{ cell === 'X' ? 'Close' : cell === 'O' ? 'Circle' : '' }}
             </span>
           </div>
         </div>
@@ -63,7 +88,7 @@ export default defineComponent({
   components: {},
   setup() {
     const db = getDatabase();
-    const route = useRoute()
+    const route = useRoute();
 
     let playerId: string;
     let lobbyId = ref<string>('');
@@ -76,125 +101,152 @@ export default defineComponent({
     const playerNameInput = ref<string>();
     const changeName = (newValue: string | number | null) => {
       if (typeof newValue === 'string') {
-        playerNameInput.value = newValue
+        playerNameInput.value = newValue;
         update(playerLobbyRef, {
           name: newValue,
         });
       }
     };
 
+    //updates Player Color
+    const changeColor = () => {
+      const mySkinIndex = playerColors.indexOf(players[playerId].color);
+      const nextColor = playerColors[mySkinIndex + 1] || playerColors[0];
+      update(playerLobbyRef, {
+        color: nextColor,
+      });
+    };
+
     onMounted(() => {
       initGame();
-    })
+    });
 
-    const currentPlayer = ref('X')
+    const currentPlayer = ref('X');
     let currentPlayerRef: DatabaseReference;
     const board = ref([
-       ['','',''],
-       ['','',''],
-       ['','','']
-    ])
+      ['', '', ''],
+      ['', '', ''],
+      ['', '', ''],
+    ]);
 
     const CalculateWinner = (board: Array<string>) => {
-      const winningLines = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
+      const winningLines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+      ];
       for (let i = 0; i < winningLines.length; i++) {
-        const [a, b, c] = winningLines[i]
+        const [a, b, c] = winningLines[i];
 
         if (board[a] && board[a] === board[b] && board[a] === board[c]) {
-          return board[a]
+          return board[a];
         }
       }
-      return null
-    }
+      return null;
+    };
 
     const winner = computed(() => {
       let tempArray = board.value.reduce((acc, val) => acc.concat(val), []);
 
-      return CalculateWinner(tempArray)
-    })
+      return CalculateWinner(tempArray);
+    });
 
     const MakeMove = (x: number, y: number) => {
-      console.log(playerRef)
-      console.log(playerId)
+      console.log(playerRef);
+      console.log(playerId);
       get(child(playerLobbyRef, 'sign')).then((snapshot) => {
         if (snapshot.exists()) {
           if (snapshot.val() === currentPlayer.value) {
-            console.log('MAKEMOVE:', playerRef)
-            if (winner.value) return
+            console.log('MAKEMOVE:', playerRef);
+            if (winner.value) return;
 
-            if (board.value[x][y] !== '') return
+            if (board.value[x][y] !== '') return;
 
-            board.value[x][y] = currentPlayer.value
+            board.value[x][y] = currentPlayer.value;
 
-            currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X'
+            currentPlayer.value = currentPlayer.value === 'X' ? 'O' : 'X';
 
             update(boardRef, {
-              board: board.value
+              board: board.value,
             });
             update(currentPlayerRef, {
-              currentPlayer: currentPlayer.value
+              currentPlayer: currentPlayer.value,
             });
           }
-        }})
-
-    }
+        }
+      });
+    };
 
     const ResetGame = () => {
       board.value = [
-        ['','',''],
-        ['','',''],
-        ['','','']
-      ]
-      currentPlayer.value = 'X'
-    }
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+      ];
+      currentPlayer.value = 'X';
+      set(boardRef, {
+        board: board.value,
+      });
+      set(currentPlayerRef, {
+        currentPlayer: currentPlayer.value,
+      });
+    };
 
     const initGame = () => {
       if (route.params.lobbyId) {
-        lobbyId.value = route.params.lobbyId.toString()
+        lobbyId.value = route.params.lobbyId.toString();
 
-        const allPlayersRef = storageRef(db, `lobbys/${lobbyId.value}/players`)
+        const allPlayersRef = storageRef(db, `lobbys/${lobbyId.value}/players`);
         boardRef = storageRef(db, `lobbys/${lobbyId.value}/board/`);
-        currentPlayerRef = storageRef(db, `lobbys/${lobbyId.value}/currentPlayer/`);
+        currentPlayerRef = storageRef(
+          db,
+          `lobbys/${lobbyId.value}/currentPlayer/`
+        );
 
         //fires when change occurs
         onValue(allPlayersRef, (snapshot) => {
-          players = snapshot.val() || {}
+          players = snapshot.val() || {};
           Object.keys(players).forEach((key) => {
-            const characterState = players[key] as unknown as DatabaseEntry
-            console.log('characterState', characterState)
-          })
-        })
+            const characterState = players[key] as unknown as DatabaseEntry;
+            console.log('characterState', characterState);
+          });
+        });
         onValue(boardRef, (snapshot) => {
-          console.log('onValue - boardRef', snapshot.val())
-          if (snapshot.val() != null){
-            board.value = snapshot.val().board
-          }
-        })
-        onValue(currentPlayerRef, (snapshot) => {
-          console.log('onValue - currentPlayerRef',snapshot.val())
+          console.log('onValue - boardRef', snapshot.val());
           if (snapshot.val() != null) {
-            currentPlayer.value = snapshot.val().currentPlayer
+            board.value = snapshot.val().board;
           }
-        })
+        });
+        onValue(currentPlayerRef, (snapshot) => {
+          console.log('onValue - currentPlayerRef', snapshot.val());
+          if (snapshot.val() != null) {
+            currentPlayer.value = snapshot.val().currentPlayer;
+          }
+        });
         //fires when a new node is added to the db
         onChildAdded(allPlayersRef, (snapshot) => {
-          const addedPlayer = snapshot.val()
-          console.log('addedPlayer',addedPlayer)
-        })
+          const addedPlayer = snapshot.val();
+          console.log('addedPlayer', addedPlayer);
+        });
         //remove character DOM Element when they leave
         onChildRemoved(allPlayersRef, (snapshot) => {
           const removedKey = snapshot.val().id;
-          console.log('removedKey', removedKey)
+          console.log('removedKey', removedKey);
         });
 
         set(boardRef, {
-          board: board.value
+          board: board.value,
         });
         set(currentPlayerRef, {
-          currentPlayer: currentPlayer.value
+          currentPlayer: currentPlayer.value,
         });
       }
-    }
+    };
 
     //*****firebase stuff*****
     const auth = getAuth();
@@ -207,9 +259,9 @@ export default defineComponent({
         const errorMessage = error.message;
         console.log(
           'So sorry, something went wrong! errorCode: ' +
-          errorCode +
-          ' | errorMessage: ' +
-          errorMessage
+            errorCode +
+            ' | errorMessage: ' +
+            errorMessage
         );
       });
 
@@ -223,41 +275,46 @@ export default defineComponent({
         playerNameInput.value = name;
 
         playerRef = storageRef(db, 'players' + playerId);
-        get(child(playerRef, 'lobbyId')).then((snapshot) => {
-          if (snapshot.exists()) {
-            lobbyId.value = snapshot.val();
-          } else {
-            lobbyId.value = route.params.lobbyId.toString()
-            console.log('Joined Lobby ID:', lobbyId.value);
-          }
-        }).catch((error) => {
-          console.error(error);
-        }).then(() => {
-          console.log('>>>>>>>>>', lobbyId.value)
-          playerLobbyRef = storageRef(db, `lobbys/${lobbyId.value}/players/` + playerId)
-          if (Object.keys(players).length === 1) {
-            update(playerLobbyRef, {
-              id: playerId,
-              name: playerNameInput.value,
-              sign: 'X',
-              gamesWon: 0
-            });
-          } else {
-            update(playerLobbyRef, {
-              id: playerId,
-              name: playerNameInput.value,
-              sign: 'O',
-              gamesWon: 0
-            });
-          }
-
-        }
-        )
+        get(child(playerRef, 'lobbyId'))
+          .then((snapshot) => {
+            if (snapshot.exists()) {
+              lobbyId.value = snapshot.val();
+            } else {
+              lobbyId.value = route.params.lobbyId.toString();
+              console.log('Joined Lobby ID:', lobbyId.value);
+            }
+          })
+          .catch((error) => {
+            console.error(error);
+          })
+          .then(() => {
+            console.log('>>>>>>>>>', lobbyId.value);
+            playerLobbyRef = storageRef(
+              db,
+              `lobbys/${lobbyId.value}/players/` + playerId
+            );
+            if (Object.keys(players).length === 1) {
+              update(playerLobbyRef, {
+                id: playerId,
+                name: playerNameInput.value,
+                sign: 'X',
+                gamesWon: 0,
+                color: randomFromArray(playerColors),
+              });
+            } else {
+              update(playerLobbyRef, {
+                id: playerId,
+                name: playerNameInput.value,
+                sign: 'O',
+                gamesWon: 0,
+                color: randomFromArray(playerColors),
+              });
+            }
+          });
 
         //remove Player from Firebase, when disconnect
         onDisconnect(playerRef).remove();
         onDisconnect(playerLobbyRef).remove();
-
       } else {
         // User is signed out
         // ...
@@ -309,27 +366,30 @@ export default defineComponent({
       return `${prefix} ${animal}`;
     };
 
+    // Options for Player Colors... these are in the same order as our sprite sheet
+    const playerColors = ['blue', 'red', 'orange', 'yellow', 'green', 'purple'];
+
     return {
-    currentPlayer,
-    board,
-    winner,
-    playerNameInput,
-    MakeMove ,
-    ResetGame,
-    changeName,
-      createName
+      currentPlayer,
+      board,
+      winner,
+      playerNameInput,
+      MakeMove,
+      ResetGame,
+      changeName,
+      changeColor,
     };
   },
 });
 </script>
 
 <style lang="scss" scoped scss>
-.main-container{
+.main-container {
   padding-top: 8px;
   text-align: center;
   color: white;
   min-height: 100vh;
-  .main-container_h1{
+  .main-container_h1 {
     margin-bottom: 8px;
     font-size: 30px;
     line-height: 36px;
@@ -346,9 +406,9 @@ export default defineComponent({
     flex-direction: column;
     align-items: center;
     margin-bottom: 8px;
-    .board_div{
+    .board_div {
       display: flex;
-      .div_cell{
+      .div_cell {
         display: flex;
         align-items: center;
         justify-content: center;
@@ -357,19 +417,19 @@ export default defineComponent({
         height: 80px;
         cursor: pointer;
 
-        &:hover{
+        &:hover {
           background-color: #86aeff;
         }
       }
     }
   }
   .main-container_bottom {
-    .bottom_h2{
+    .bottom_h2 {
       margin-bottom: 8px;
       font-size: 60px;
       line-height: 1;
     }
-    .bottom_resetBtn{
+    .bottom_resetBtn {
       background-color: rgb(241, 157, 0);
       color: white;
     }
@@ -378,11 +438,7 @@ export default defineComponent({
 .material-symbols-outlined {
   font-size: 70px;
   line-height: 70px;
-  font-variation-settings:
-    'FILL' 0,
-    'wght' 400,
-    'GRAD' 0,
-    'opsz' 48
+  font-variation-settings: 'FILL' 0, 'wght' 400, 'GRAD' 0, 'opsz' 48;
 }
 .player-info {
   position: absolute;
@@ -400,7 +456,6 @@ label {
 }
 
 input[type='text'],
-
 input[type='text'] {
   outline: 0;
   padding-left: 0.5em;
